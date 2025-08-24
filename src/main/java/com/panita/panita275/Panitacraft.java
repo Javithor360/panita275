a983951ef1faa8d920f1d2c6707737bdf63f1901
Javithor360 <floresmejia004@gmail.com>
2025-08-23 18:42:28 -0600

    feat, core: define main class execution and registries

--- src/main/java/com/panita/panita275/Panitacraft.java
+++ src/main/java/com/panita/panita275/Panitacraft.java
@@ -1,17 +1,44 @@
 package com.panita.panita275;
 
+import com.panita.panita275.core.commands.CommandRegistry;
+import com.panita.panita275.core.listeners.ListenerRegistry;
+import com.panita.panita275.core.modules.PluginModule;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+
 public final class Panitacraft extends JavaPlugin {
+    private final List<PluginModule> modules = List.of();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("Panitacraft is starting up!");
 
+        // Register commands
+        CommandRegistry cmdRegistry = new CommandRegistry(this);
+        ListenerRegistry listenerRegistry = new ListenerRegistry(this, getConfig());
+
+        for (PluginModule module : modules) {
+            getLogger().info("[Panitacraft Core] Enabling module: " + module.name());
+
+            // Register commands and listeners for the module
+            cmdRegistry.registerAll(module.commandPackage());
+            listenerRegistry.registerAll(module.listenerPackage());
+
+            // Additional module-specific initialization can go here
+            module.onEnable(this);
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("Panitacraft is shutting down!");
+
+        for (PluginModule module : modules) {
+            getLogger().info("[Panitacraft Core] Disabling module: " + module.name());
+            module.onDisable(this);
+        }
     }
 }
