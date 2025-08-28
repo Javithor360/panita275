dc6e2e8a8ce4f1993c286b8edf281a0213f21378
Javithor360 <floresmejia004@gmail.com>
2025-08-28 07:53:45 -0600

    fix, optimization: remove entity spawn detector NMS

--- src/main/java/com/panita/panita275/optimization/listeners/MobSpawnListener.java
+++ src/main/java/com/panita/panita275/optimization/listeners/MobSpawnListener.java
@@ -3,36 +3,13 @@ package com.panita.panita275.optimization.listeners;
 import com.panita.panita275.Panitacraft;
 import com.panita.panita275.core.chat.Messenger;
 import com.panita.panita275.core.config.ConfigDefaults;
-import org.bukkit.craftbukkit.v1_21_R1.CraftWorld;
-import org.bukkit.entity.Bee;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Wolf;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.level.levelgen.structure.Structure;
-import net.minecraft.world.level.levelgen.structure.StructureStart;
-
 public class MobSpawnListener implements Listener {
-    private boolean isInDatapackStructure(LivingEntity entity, String namespace) {
-        var worldHandle = ((CraftWorld) entity.getWorld()).getHandle();
-        var pos = new BlockPos(entity.getLocation().getBlockX(),
-                entity.getLocation().getBlockY(),
-                entity.getLocation().getBlockZ());
-        var structureManager = worldHandle.getStructureManager();
-
-        for (ResourceKey<Structure> key : Registry.STRUCTURE.keySet()) {
-            if (!key.location().getNamespace().equals(namespace)) continue;
-
-            StructureStart start = structureManager.getStructureStart(pos, key);
-            if (start != null && start.isValid()) return true;
-        }
-        return false;
-    }
 
     @EventHandler
     public void onCreatureSpawn(CreatureSpawnEvent event) {
