a65445a87f04ad8dc94e92ecfc46746bea967c74
Javithor360 <floresmejia004@gmail.com>
2025-09-26 02:04:19 -0600

    feat, core: add some methods for item utils

--- src/main/java/com/panita/panita275/core/util/ItemUtils.java
+++ src/main/java/com/panita/panita275/core/util/ItemUtils.java
@@ -1,8 +1,12 @@
 package com.panita.panita275.core.util;
 
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
 
 public class ItemUtils {
     public static boolean checkItemModel(ItemStack item, NamespacedKey modelKey) {
@@ -18,5 +22,22 @@ public class ItemUtils {
 
         ItemMeta meta = item.getItemMeta();
         return meta.getItemModel();
+    }
+
+    public static int countMaterial(Player player, Material mat) {
+        return Arrays.stream(player.getInventory().getContents())
+                .filter(i -> i != null && i.getType() == mat)
+                .mapToInt(ItemStack::getAmount)
+                .sum();
+    }
+
+    public static void removeMaterial(Player player, Material mat, int amount) {
+        for (ItemStack stack : player.getInventory().getContents()) {
+            if (stack == null || stack.getType() != mat) continue;
+            int take = Math.min(amount, stack.getAmount());
+            stack.setAmount(stack.getAmount() - take);
+            amount -= take;
+            if (amount <= 0) break;
+        }
     }
 }
