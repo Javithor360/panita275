b71ff06f7780282a715e9530031fd6a5f3d92f02
Javithor360 <floresmejia004@gmail.com>
2025-09-26 02:08:29 -0600

    feat, troll: implement listener to switch flight power for firework rocket

--- src/main/java/com/panita/panita275/troll/listeners/RechargeableFirework.java
+++ src/main/java/com/panita/panita275/troll/listeners/RechargeableFirework.java
@@ -75,4 +75,66 @@ public class RechargeableFirework implements Listener {
         item.setData(DataComponentTypes.DAMAGE, newDamage);
         Messenger.prefixedSend(player, "&7Has recargado tu cohete con &b" + toConsume + " &7de pólvora. Energía actual: &b" + (currentEnergy + toConsume) + "/" + maxEnergy);
     }
+
+    /**
+     * This handles the logic to change the firework rocket level
+     * changing dynamically its power and lore, also adding a cooldown
+     * to prevent spam clicking
+     */
+    @EventHandler
+    public void onRechargeableRocketLevelChange(PlayerInteractEvent event) {
+        if (event.getItem() == null) return;
+
+        Player player = event.getPlayer();
+        Action action = event.getAction();
+
+        // The condition here is that the player just left clicks the air
+        // and is not sneaking or gliding
+        if (action != Action.LEFT_CLICK_AIR) return;
+        if (player.isSneaking() || player.isGliding()) return;
+
+        // Ensure the item is a rechargeable firework rocket and the player has no
+        // cooldown and is not in creative
+        ItemStack item = event.getItem();
+        if (!CustomItemManager.isCustomItem(item, "rechargeable_firework")) return;
+        if (player.hasCooldown(item) || player.getGameMode() == GameMode.CREATIVE) return;
+
+        // Then get the current level, and retrieve the persistent data container
+        FireworkMeta meta = (FireworkMeta) item.getItemMeta();
+
+        PersistentDataContainer container = meta.getPersistentDataContainer();
+        int currentLevel = container.getOrDefault(ROCKET_LEVEL_KEY, PersistentDataType.INTEGER, 1);
+        int nextLevel = (currentLevel % 5) + 1; // Cycle through levels 1 to 5
+
+        container.set(ROCKET_LEVEL_KEY, PersistentDataType.INTEGER, nextLevel); // Update the level in the persistent data
+
+        // Update the firework power and lore based on the new level
+        meta.setPower(nextLevel);
+
+        List<Component> lore = meta.lore() != null ? new ArrayList<>(meta.lore()) : new ArrayList<>();
+        String levelText = switch (nextLevel) {
+            case 1 -> "<#a0fc97>Nivel 1";
+            case 2 -> "<#fffa61>Nivel 2";
+            case 3 -> "<#fcc072>Nivel 3";
+            case 4 -> "<#ff8161>Nivel 4";
+            case 5 -> "<#ff4747>Nivel 5";
+            default -> "<#a0fc97>Nivel ¿?";
+        };
+
+        if (!lore.isEmpty() && lore.getFirst().toString().contains("Poder de Vuelo")) {
+            lore.set(0, Messenger.mini("<!italic><#9c1a1a>\uD83D\uDCA5</#9c1a1a> <#fc993d>Poder de Vuelo:</#fc993d> " + levelText));
+        } else {
+            lore.addFirst(Messenger.mini("<!italic><#9c1a1a>\uD83D\uDCA5</#9c1a1a> <#fc993d>Poder de Vuelo:</#fc993d> " + levelText));
+        }
+
+        meta.lore(lore);
+
+        // Apply a short cooldown to prevent rapid usage
+        Bukkit.getScheduler().runTaskLater(Panitacraft.getInstance(), () -> {
+            player.setCooldown(item, 1 * 20);
+        }, 1L);
+
+        item.setItemMeta(meta);
+        Messenger.prefixedSend(player, "&7El &bPoder de Vuelo &7del cohete cambiado a &b" + levelText + "&7.");
+    }
 }
